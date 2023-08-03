a44ba598a4c6b114da996b7b4d04d8edbbeeb369
Callum <callumnm195@gmail.com>
2023-08-03 21:37:26 +0100

    touching up the new stuff - adding pausing to videos not on display

--- scripts/animateBackground.js
+++ scripts/animateBackground.js
@@ -2,6 +2,9 @@ const content = document.querySelector('.backgroundContent')
 const sliderButton = document.getElementById('slider-checkbox');
 const sliderButtonIcon = document.getElementById('slider-checkbox-icon');
 
+const backgroundVideo = document.getElementById('background-video');
+
+
 function toggleBackground() {
     content.classList.toggle('fade-background');
 }
@@ -12,10 +15,25 @@ sliderButton.addEventListener('change', function() {
     
     if (this.checked) {
         sliderButtonIcon.src = "/assets/right-arrow.png";
+        pauseSubVideos();
     } else {
         sliderButtonIcon.src = "/assets/left-arrow.png";
+        pauseHomeVideo();
     }
 });
 
+function pauseHomeVideo() {
+    backgroundVideo.pause();
+    displayVideo1.play();
+    displayVideo2.play();
+    displayVideo3.play();
+}
+
+function pauseSubVideos() {
+    backgroundVideo.play();
+    displayVideo1.pause();
+    displayVideo2.pause();
+    displayVideo3.pause();
+}
 
 setInterval(toggleBackground, 3000); // Adjust the interval (in milliseconds) as needed
