92595806f25dd62b11e3f199cfe661b6c0fac2cf
Callum <callumnm195@gmail.com>
2023-08-04 17:46:34 +0100

    some good progress on this

--- scripts/cinemaController.js
+++ scripts/cinemaController.js
@@ -3,6 +3,7 @@ const fullscreenButton = document.getElementById('fullscreenButton');
 const curtainButton = document.getElementById('curtain-checkbox');
 const curtainButtonIcon = document.getElementById('curtain-checkbox-icon');
 const cinemaChangeButton = document.getElementById('cinemaChangeButton');
+const videoCollectionCinema = document.getElementById('video-collection-cinema');
 
 fullscreenButton.addEventListener('click', toggleFullscreen);
 
@@ -12,11 +13,26 @@ const visualSources = [
   'Two Dangers THE WARNINGS.mp4',
 ];
 
+const visualSourcesThumbnails = [
+  'MID OCEAN.jpg',
+  'Abstrkt Replace Mode.jpg',
+  'Two Dangers THE WARNINGS.jpg',
+];
+
 const filmSources = [
   'home page.mp4'
 ]
 
+const filmSourcesThumbnails = [
+  'Two Dangers THE WARNINGS.jpg',
+  'Abstrkt Replace Mode.jpg',
+]
+
+const visualId = 1;
+const filmId = 2;
+
 let currentVideoIndex = 0;
+let currentTypeIndex = 1;
 
 function toggleFullscreen() {
   if (!document.webkitFullscreenElement) {
@@ -28,18 +44,23 @@ function toggleFullscreen() {
   }
 }
 
-function changeMainVideo(videoId) {
-    if(currentVideoIndex !== videoId) {
-      currentVideoIndex = videoId;
+function changeMainVideo(typeID, videoId) {
+  if((currentVideoIndex !== videoId) || (currentTypeIndex !== typeID)) {
+    currentVideoIndex = videoId;
+    if(currentTypeIndex == 1) {
       videoPlayer.src = "".concat('/assets/', visualSources[currentVideoIndex]);
-      videoPlayer.play();
+    } else {
+      videoPlayer.src = "".concat('/assets/', filmSources[currentVideoIndex]);
     }
-    
+    playVideo();
+  } else if (curtainButton.checked == true) {
+    playVideo();
+  }
 }
 
 videoPlayer.addEventListener('ended', () => {
-    currentVideoIndex = (currentVideoIndex + 1) % visualSources.length;
-    changeMainVideo(currentVideoIndex);
+  currentVideoIndex = (currentVideoIndex + 1) % visualSources.length;
+  changeMainVideo(currentVideoIndex);
 });
 
 curtainButton.addEventListener('change', function() {
@@ -53,9 +74,53 @@ curtainButton.addEventListener('change', function() {
 });
 
 function playVideo() {
+  if (curtainButton.checked) {
+    curtainButton.checked = false;
+  }
   videoPlayer.play()
 }
 
 function pauseVideo() {
   videoPlayer.pause()
 }
+
+function createThumbnails() {
+  videoCollectionCinema.innerHTML = ''; // Clear the current thumbnails
+  if (currentTypeIndex == 1) {
+    for (let i = 0; i < visualSourcesThumbnails.length; i++) {
+      let thumbnail = document.createElement('div');
+      thumbnail.classList.add('thumbnail');
+      let img = document.createElement('img');
+      img.src = "".concat('/assets/',visualSourcesThumbnails[i]);
+      thumbnail.setAttribute('onclick', "".concat('changeMainVideo(', currentTypeIndex ,', ' , i, ')'))
+      videoCollectionCinema.appendChild(thumbnail);
+      thumbnail.appendChild(img);
+    }
+    videoPlayer.muted = true;
+    videoPlayer.controls = false;
+    fullscreenButton.style.zIndex = 10;
+    
+  } else {
+    for (let i = 0; i < filmSourcesThumbnails.length; i++) {
+      let thumbnail = document.createElement('div');
+      thumbnail.classList.add('thumbnail');
+      let img = document.createElement('img');
+      img.src = "".concat('/assets/',filmSourcesThumbnails[i]);
+      thumbnail.setAttribute('onclick', "".concat('changeMainVideo(', currentTypeIndex ,', ' , i, ')'))
+      videoCollectionCinema.appendChild(thumbnail);
+      thumbnail.appendChild(img);
+    }
+    videoPlayer.muted = false;
+    videoPlayer.controls = true;
+    fullscreenButton.style.zIndex = -5;
+  }
+}
+
+// Button click event handler
+cinemaChangeButton.addEventListener('click', () => {
+  currentTypeIndex = currentTypeIndex === 1 ? 2 : currentTypeIndex === 2 ? 1 : currentTypeIndex;
+  curtainButton.checked = true;
+  createThumbnails();
+});
+
+createThumbnails();
