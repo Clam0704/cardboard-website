28bafe932bd499c5ba45be7e373aba40f40bb8cb
Callum <callumnm195@gmail.com>
2023-08-17 18:11:26 +0100

    more changes to about page and added socials to the nav bar

--- scripts/animateAbout.js
+++ scripts/animateAbout.js
@@ -3,16 +3,35 @@ const slideElements = document.querySelectorAll('.slide-text');
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.style.animationPlayState = 'running';
-      if(entry.target.id == "last-slide") {
-        let slideBottom = document.getElementsByClassName('slide-bottom')[0];
-        slideBottom.style.animationPlayState = 'running';
-      }
+        console.log("here");
+        entry.target.style.animationPlayState = 'running';
+        if(entry.target.id == "last-slide") {
+            let slideBottom = document.getElementsByClassName('slide-bottom')[0];
+            slideBottom.style.animationPlayState = 'running';
+        }
+    } else {
+        entry.target.style.animationPlayState = 'running';
+        let anim = entry.target.style.animationName;
+        entry.target.style.animationName = "none";
+        entry.target.offsetWidth;
+        entry.target.style.animationName = anim;
+        console.log("here2")
+        entry.target.style.animationPlayState = 'paused';
+        if(entry.target.id == "last-slide") {
+            let slideBottom = document.getElementsByClassName('slide-bottom')[0];
+            slideBottom.style.animationPlayState = 'running';
+            let anim = slideBottom.style.animationName;
+            slideBottom.style.animationName = "none";
+            slideBottom.offsetWidth;
+            slideBottom.style.animationName = anim;
+            console.log("here2")
+            slideBottom.style.animationPlayState = 'paused';
+          }
     }
   });
-}, { threshold: 0.01 }); // Change the threshold value as needed
+}, { threshold: 0.1 }); // Change the threshold value as needed
 
 slideElements.forEach(element => {
-  observer.observe(element);
-  console.log("beep")
+    observer.observe(element);
+    console.log("beep")
 });
